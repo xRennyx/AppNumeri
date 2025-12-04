de1b01848a63a50d8f1206cb21e0741cdde3eed5
xRennyx <150335643+xRennyx@users.noreply.github.com>
2025-12-04 20:59:29 +0100

    Update prove.php

--- php/prove.php
+++ php/prove.php
@@ -39,7 +39,7 @@ echo 'Dimensione vettore: '.$n;
 echo '<br>';
 echo 'Elementi vettore: <br>';
 for($i=0; $i<$n; $i++)
-echo ' '.$vett[$i];
+    echo ' '.$vett[$i];
 echo '<br>';
 print_r($vett);
 echo '<br>';
@@ -111,7 +111,7 @@ echo '<br>';
 echo $valori[1];
 echo '<br>';
 if(array_key_exists('eta', $studente))
-echo $studente['eta'];
+    echo $studente['eta'];
 else echo 'Eta non presente';
 echo '<br>';
 $var1=5;
@@ -148,3 +148,111 @@ if(empty($var5))//ritorna vero se è zero
     echo 'var5 è vuota';
 else echo 'non è vuota';
 echo '<br>';
+// array_keys()
+// Ritorna tutte le chiavi dell'array
+$chiavi = array_keys($studente);
+echo "Chiavi array studente: ";
+print_r($chiavi);
+echo "<br>";
+
+// array_values()
+// Ritorna tutti i valori dell'array
+$valori = array_values($studente);
+echo "Valori array studente: ";
+print_r($valori);
+echo "<br>";
+
+// array_key_exists()
+// Controlla se una chiave esiste nell'array
+if(array_key_exists('nome', $studente))
+    echo "La chiave 'nome' esiste<br>";
+else
+    echo "La chiave 'nome' NON esiste<br>";
+
+// isset()
+// Controlla se una variabile esiste e non è null
+if(isset($studente['eta']))
+    echo "Variabile esiste (isset)<br>";
+else
+    echo "Variabile NON esiste (isset)<br>";
+
+// in_array()
+// Controlla se un valore è presente nell'array
+if(in_array(20, $vett2))
+    echo "20 è presente in vett2<br>";
+else
+    echo "20 NON è presente in vett2<br>";
+
+// array_search()
+// Restituisce la chiave/indice del valore cercato
+$pos = array_search(20, $vett2);
+echo "20 si trova alla posizione: $pos<br>";
+
+// unset()
+// Elimina un elemento dell'array
+unset($vett2[2]);
+echo "vett2 dopo unset: " . implode(' ', $vett2) . "<br>";
+
+// array_merge()
+// Unisce due array
+$unito = array_merge($vett, $vett2);
+echo "Array unito: " . implode(' ', $unito) . "<br>";
+
+// asort()
+// Ordina l'array mantenendo le chiavi
+asort($vett2);
+echo "vett2 ordinato (asort): " . implode(' ', $vett2) . "<br>";
+
+// arsort()
+// Ordina al contrario mantenendo le chiavi
+arsort($vett2);
+echo "vett2 ordinato inverso (arsort): " . implode(' ', $vett2) . "<br>";
+
+// ksort()
+// Ordina un array in base alle chiavi crescenti
+ksort($studente);
+echo "studente ordinato per chiavi (ksort): ";
+print_r($studente);
+echo "<br>";
+
+// krsort()
+// Ordina un array in base alle chiavi decrescenti
+krsort($studente);
+echo "studente ordinato per chiavi inverse (krsort): ";
+print_r($studente);
+echo "<br>";
+
+// array_map()
+// Applica una funzione a tutti gli elementi dell'array
+$moltiplicato = array_map(function($n){ return $n*2; }, $vett);
+echo "vett moltiplicato x2 (array_map): ";
+print_r($moltiplicato);
+echo "<br>";
+
+// array_filter()
+// Filtra gli elementi (tiene solo quelli > 15)
+$filtrato = array_filter($vett2, function($n){ return $n > 15; });
+echo "vett2 filtrato (>15): ";
+print_r($filtrato);
+echo "<br>";
+
+// array_walk()
+// Esegue una funzione su ogni elemento
+array_walk($vett, function($valore, $chiave){
+    echo "Elemento $chiave vale $valore<br>";
+});
+
+// array_slice()
+// Estrae una parte dell'array SENZA modificarlo
+$parte = array_slice($vett, 1, 2);
+echo "Parte di vett (slice 1,2): ";
+print_r($parte);
+echo "<br>";
+
+// array_splice()
+// Rimuove una parte dell’array e può sostituirla
+$vett_copia = $vett;
+array_splice($vett_copia, 1, 1, ['X','Y']);
+echo "vett dopo splice (1 elemento sostituito con X,Y): ";
+print_r($vett_copia);
+echo "<br>";
