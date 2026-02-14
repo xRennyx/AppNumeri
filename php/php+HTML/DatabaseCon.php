d60c19c76f0c0d62a34458ce8abe0680b40b528f
xRennyx <150335643+xRennyx@users.noreply.github.com>
2026-02-14 11:21:25 +0100

    Update DatabaseCon.php

--- php/php+HTML/DatabaseCon.php
+++ php/php+HTML/DatabaseCon.php
@@ -1,11 +1,17 @@
 <?php
-
 class DatabaseCon
 {
     private static PDO $db;
     public static function getDB(array $dbconfig):PDO
     {
-        self:: $db=new PDO($dbconfig['dsn'], $dbconfig['username'], $dbconfig['password'], $dbconfig['options']);
+        if(!isset(self::$db))
+        {
+            try {
+                self:: $db = new PDO($dbconfig['dsn'], $dbconfig['username'], $dbconfig['password'], $dbconfig['options']);
+            }catch (PDOException $e){
+                return self::$db=null;
+            }
+        }
         return self::$db;
     }
 }
