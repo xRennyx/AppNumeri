87fa3a856957c2b857753c5181ea75aceb275ab1
xRennyx <150335643+xRennyx@users.noreply.github.com>
2026-02-21 11:23:00 +0100

    Add files via upload

---
+++ php/templete/index.php
@@ -0,0 +1,3 @@
+<?php
+$message = "Hello World!";
+require 'layout.tpl.php'; //come include di C richiedo il file
---
+++ php/templete/index2.php
@@ -0,0 +1,3 @@
+<?php
+$message = "Ciao a Tutti";
+require 'layout.tpl.php'; //come include di C richiedo il file
