ca3744c7d9c61f0f0dfd90db7ff9d714cf381afb
xRennyx <150335643+xRennyx@users.noreply.github.com>
2026-04-16 22:01:12 +0200

    Update FastRoute.sql

--- php/FastRoute/FastRoute.sql
+++ php/FastRoute/FastRoute.sql
@@ -1,26 +1,55 @@
 create database if not exists francesco_renesto_fastroute
-
 use database francesco_renesto_fastroute
 
-create table corrieri
-(
-	matricola varchar(50) primary key,
-	nome varchar(50),
-	cognome varchar(50)
+CREATE TABLE Cliente_Mittente (
+    cf VARCHAR(16) PRIMARY KEY,
+    nome VARCHAR(50),
+    cognome VARCHAR(50),
+    punti_fedelta INT
 );
 
-create table destinatari
-(
-	id_destinatario int PRIMARY key auto_increment,
-	nome varchar(50),
-	cognome varchar(50)
+CREATE TABLE Corriere (
+    matricola INT PRIMARY KEY,
+    nome VARCHAR(50),
+    cognome VARCHAR(50)
 );
 
-create table plichi
-(
-	codice int primary key auto_increment,
-	matricola_corriere varchar(50),
-	id_destinatario int,
-	foreign key (matricola_corriere) references corrieri(matricola),
-	foreign key (id_destinatario) references destinatari(id_destinatario)
+
+CREATE TABLE Destinatario (
+    cf VARCHAR(16) PRIMARY KEY,
+    nome VARCHAR(50),
+    cognome VARCHAR(50)
+);
+
+CREATE TABLE Cliente (
+    cf VARCHAR(16) PRIMARY KEY,
+    punti_fedelta INT,
+    FOREIGN KEY (cf) REFERENCES Destinatario(cf)
+);
+
+CREATE TABLE No_Cliente (
+    cf VARCHAR(16) PRIMARY KEY,
+    FOREIGN KEY (cf) REFERENCES Destinatario(cf)
+);
+
+CREATE TABLE Plico (
+    codice INT PRIMARY KEY,
+    data_spedizione DATE,
+    matricola_corriere INT UNIQUE, 
+    cf_destinatario VARCHAR(16) UNIQUE,
+    data_ritiro DATE,
+    
+    FOREIGN KEY (matricola_corriere) REFERENCES Corriere(matricola),
+    FOREIGN KEY (cf_destinatario) REFERENCES Destinatario(cf)
+);
+
+CREATE TABLE Incarica (
+    cf_cliente VARCHAR(16),
+    matricola_corriere INT,
+    data_accettazione DATE,
+    
+    PRIMARY KEY (cf_cliente, matricola_corriere, data_accettazione),
+    
+    FOREIGN KEY (cf_cliente) REFERENCES Cliente_Mittente(cf),
+    FOREIGN KEY (matricola_corriere) REFERENCES Corriere(matricola)
 );
